f1e3471ac4e78ec575eca56093f9ea52e9029ed1
AlexNazemi <alexandernazemi@gmail.com>
2022-03-12 18:04:10 +0000

    TASK-002-Create firebase config file in the src file with the firebase config object inside-AN

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDFr4-sOrEEJam97HXEtoTZs2tQVi0l1NI",
+    authDomain: "facebook-ui-clone-534bb.firebaseapp.com",
+    projectId: "facebook-ui-clone-534bb",
+    storageBucket: "facebook-ui-clone-534bb.appspot.com",
+    messagingSenderId: "634130700498",
+    appId: "1:634130700498:web:69aaae6a66c50a8d72750b",
+    measurementId: "G-XL6ZQFP7GP"
+  };
